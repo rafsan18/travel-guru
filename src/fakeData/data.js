b1df0035174f312eead2c0f6cae2b82a1d51fcde
rafsan <rafsanamin18@gmail.com>
2020-09-16 23:41:52 +0600

    first commit

---
+++ src/fakeData/data.js
@@ -0,0 +1,92 @@
+var data = [
+  {
+    name: "Cox's Bazar",
+    info:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Molestias facere reiciendis soluta voluptate a minima ratione ut doloremque fuga mollitia.",
+    detailInfo:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel et impedit molestiae laborum tempore suscipit in fugit ex, iure odio saepe cumque quibusdam assumenda odit velit quisquam culpa amet illo placeat itaque, est unde recusandae sed? Fugit totam quas maiores.",
+    img: "https://i.ibb.co/1bFg2yH/Rectangle-1.png",
+    id: 1,
+    category: "destination",
+  },
+  {
+    name: "Sreemangal",
+    info:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Molestias facere reiciendis soluta voluptate a minima ratione ut doloremque fuga mollitia.",
+    detailInfo:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel et impedit molestiae laborum tempore suscipit in fugit ex, iure odio saepe cumque quibusdam assumenda odit velit quisquam culpa amet illo placeat itaque, est unde recusandae sed? Fugit totam quas maiores.",
+    img: "https://i.ibb.co/HGCK0sq/Sreemongol.png",
+    id: 2,
+    category: "destination",
+  },
+  {
+    name: "Sundarbans",
+    info:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Molestias facere reiciendis soluta voluptate a minima ratione ut doloremque fuga mollitia.",
+    detailInfo:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel et impedit molestiae laborum tempore suscipit in fugit ex, iure odio saepe cumque quibusdam assumenda odit velit quisquam culpa amet illo placeat itaque, est unde recusandae sed? Fugit totam quas maiores.",
+    img: "https://i.ibb.co/sQVmZP0/sundorbon.png",
+    id: 3,
+    category: "destination",
+  },
+  {
+    name: "Sajek",
+    info:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Molestias facere reiciendis soluta voluptate a minima ratione ut doloremque fuga mollitia.",
+    detailInfo:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel et impedit molestiae laborum tempore suscipit in fugit ex, iure odio saepe cumque quibusdam assumenda odit velit quisquam culpa amet illo placeat itaque, est unde recusandae sed? Fugit totam quas maiores.",
+    img: "https://i.ibb.co/K2NRJGJ/Sajek.png",
+    id: 4,
+    category: "destination",
+  },
+  {
+    title: "Light Bright airy stylish apt & safe peaceful stay",
+    rating: 4.9,
+    price: 34,
+    img: "https://i.ibb.co/BCtQK5K/Rectangle-26.png",
+    id: 5,
+    numberOfPersonRated: 20,
+    guests: 4,
+    bedrooms: 2,
+    beds: 2,
+    bath: 2,
+    total: 167,
+    info: "With air conditioning kitchen.",
+    cancellation: "Cancellation flexibility available.",
+    category: "hotelInfo",
+  },
+  {
+    title: "Apartment in Lost Panorama",
+    rating: 4.8,
+    price: 52,
+    img: "https://i.ibb.co/mbcRHRB/Rectangle-27.png",
+    id: 6,
+    numberOfPersonRated: 10,
+    guests: 4,
+    bedrooms: 2,
+    beds: 2,
+    bath: 2,
+    total: 167,
+    info: "With air conditioning kitchen.",
+    cancellation: "Cancellation flexibility available.",
+    category: "hotelInfo",
+  },
+  {
+    title: "AR Lounge & Pool (r&r + b&b)",
+    rating: 4.9,
+    price: 44,
+    img: "https://i.ibb.co/g6pT27m/Rectangle-28.png",
+    id: 7,
+    numberOfPersonRated: 25,
+    guests: 4,
+    bedrooms: 2,
+    beds: 2,
+    bath: 2,
+    total: 167,
+    info: "With air conditioning kitchen.",
+    cancellation: "Cancellation flexibility available.",
+    category: "hotelInfo",
+  },
+];
+
+export default data;
